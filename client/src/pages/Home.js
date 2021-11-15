f4c90e3eeffb221515fb42c6af8c213c9018e683
hroddy <hannah.roddy@gmail.com>
2021-11-15 15:41:13 -0800

    fixing issues

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ProductList from '../components/ProductList';
 import CategoryMenu from '../components/CategoryMenu';
 import Cart from '../components/Cart';
-import { shoesOptions } from '../components/data/shoesOptions';
+// import { shoesOptions } from '../components/data/shoesOptions';
 
 const Home = () => {
   return (
