c26b64f2e1c51e316d1807636b61c0ad4fd416ed
hroddy <hannah.roddy@gmail.com>
2021-11-15 15:23:56 -0800

    shoeOptions in progress

---
+++ client/src/components/data/shoesOptions.js
@@ -0,0 +1,10 @@
+export const shoesOptions = [
+    {
+        name: 'slim-tread',
+        src: 'images/shop/shoes/slim-tread.JPEG'
+    },
+    {
+        name: 'tread-chelsea',
+        src: 'images/shop/shoes/tread-chelsea.JPEG'
+    }
+]
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ProductList from '../components/ProductList';
 import CategoryMenu from '../components/CategoryMenu';
 import Cart from '../components/Cart';
+import { shoesOptions } from '../components/data/shoesOptions';
 
 const Home = () => {
   return (
@@ -9,6 +10,12 @@ const Home = () => {
       <CategoryMenu />
       <ProductList />
       <Cart />
+      {/* {shoesOptions.map(shoe => (
+        <div>
+          <h1>{shoe.name}</h1>
+          <img src={shoe.src} />
+        </div>
+      ))} */}
     </div>
   );
 };
