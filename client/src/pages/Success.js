eaecdc5325b7ebb3d66c041436020f24a332719d
mjos7 <markjnjoseph@gmail.com>
2021-11-18 18:00:13 -0800

    update success page text

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -32,8 +32,7 @@ function Success() {
   return (
     <div>
       <Jumbotron>
-        <h1>Success!</h1>
-        <h2>Thank you for your purchase!</h2>
+        <h1>Thank you for your purchase!</h1>
         <h2>You will now be redirected to the home page</h2>
       </Jumbotron>
     </div>
